3afe2aeca0126219b1539581173527c4e57b6b5e
conformist-mw <conformist.mw@gmail.com>
2016-10-08 19:18:16 +0300

    done the task

--- password_strength.py
+++ password_strength.py
@@ -1,7 +1,23 @@
+import re
 
 
 def get_password_strength(password):
-    pass
+    strength = 0
+    if re.search(r'[a-z]', password):
+        strength += 1
+    if re.search(r'[A-Z]', password):
+        strength += 1
+    if re.search(r'[0-9]', password):
+        strength += 1
+    if re.search(r'[ !#$%&():*+,-./[\\\]^_`{|}~]', password):
+        strength += 2
+    if len(password) > 8:
+        strength += 2
+    if len(password) > 16:
+        strength += 2
+    if re.search(r'[\w][\d][\w][\d][\w][\d]', password):
+        strength += 1
+    return strength
 
 
 if __name__ == '__main__':
